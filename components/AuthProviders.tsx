53fe18015ea1c8e5f99f4ee01935bf7dda3d91d7
pratik_guha <sguha4966@gmail.com>
2024-09-10 19:19:21 +0530

    commit

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,41 @@
+"use client"
 import React from 'react'
+import { getProviders,signIn } from 'next-auth/react'
+import { useState,useEffect } from 'react'
+
+type Provider={
+  id: string,
+  name: string,
+  type: string,
+  signinUrl: string,
+  callbackUrl: string,
+  signinUrlParams?:Record<string,string>|null
+}
+
+type Providers=Record<string,Provider>
 
 export default function AuthProviders() {
+
+  const [providers,setProviders] = useState<Providers | null>(null)
+
+  useEffect(()=>{
+    const fetchProviders=async()=>{
+      const res=await getProviders()
+      setProviders(res)
+    }
+    fetchProviders()
+  },[])
+  if(providers){
   return (
     <div>
-      AuthProviders
+      {
+        Object.values(providers).map((provider:Provider,i)=>(
+          <button key={i}>
+            {provider.id}
+          </button>
+        ))
+      }
     </div>
   )
+}
 }
